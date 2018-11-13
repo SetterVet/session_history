dc8d5caf55f12b6b074907ce8dd8c06fc4cf4301
SetterVet <kosvit1408@gmail.com>
2018-11-13 22:56:15 +0200

    test graph and try load file

--- session/models.py
+++ session/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    created = models.DateTimeField(verbose_name='create', auto_now_add=True, blank=False)
+    file = models.FileField(upload_to='uploads/')
+
+    @staticmethod
+    def create(file):
+        a=File(file)
+        a.save()
+
+
--- session/views.py
+++ session/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import File
+import csv
+def index(request):
+    if request.method=='POST':
+        inpfile=request.POST['csv']
+        File.create(inpfile)
+    else:
+        return render(request,'session/index.html')
