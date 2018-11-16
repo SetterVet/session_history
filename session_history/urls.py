86378bbbe366bed7b7b222490b8d318f027e6817
SetterVet <kosvit1408@gmail.com>
2018-11-16 10:44:47 +0200

    add correct url

--- session_history/urls.py
+++ session_history/urls.py
@@ -21,10 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="index"),
+    path('/',views.index,name="index"),
     path('result/<int:pk>/',views.result,name='result'),
 
 ]
-handler404 = views.handler404
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
